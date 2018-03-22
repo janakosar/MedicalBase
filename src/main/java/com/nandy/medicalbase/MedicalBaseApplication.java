5904ae7299fe1503f6955b8585664487174ad0bd
Jana Kosar <yana.kosar@gmail.com>
2018-03-22 17:47:39 +0200

    Initial commit

---
+++ src/main/java/com/nandy/medicalbase/MedicalBaseApplication.java
@@ -0,0 +1,12 @@
+package com.nandy.medicalbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalBaseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nandy/medicalbase/MedicalBaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nandy.medicalbase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MedicalBaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
