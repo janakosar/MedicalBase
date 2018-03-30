b535610e9641732cf464dceb83a5f4dbb5355178
Jana Kosar <yana.kosar@gmail.com>
2018-03-30 10:38:54 +0300

    Remove PatientList from router

--- medical-app/src/app/app-routing.module.ts
+++ medical-app/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from "@angular/core";
+import {Routes, RouterModule} from "@angular/router";
 import {PatientCreateComponent} from "./patients/components/patient-create/patient-create.component";
 import {PatientDetailsComponent} from "./patients/components/patient-details/patient-details.component";
-import {PatientListComponent} from "./patients/components/patient-list/patient-list.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/patients', pathMatch: 'full' },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: 'patient/create', component: PatientCreateComponent },
-  { path: 'patients/', component: PatientListComponent },
   { path: 'patient/:patientId', component: PatientDetailsComponent },
   { path: 'patient/edit/:patientId', component: PatientCreateComponent },
 
